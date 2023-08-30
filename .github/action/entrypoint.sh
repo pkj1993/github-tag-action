2b0d0b4c4e99b5794cd86e38c41277dca5f42e77
pkj1993 <140062112+pkj1993@users.noreply.github.com>
2023-08-30 12:31:12 +0530

    Update entrypoint.sh

--- .github/action/entrypoint.sh
+++ .github/action/entrypoint.sh
@@ -156,7 +156,7 @@ log=${history_type[${branch_history}]}
 printf "History:\n---\n%s\n---\n" "$log"
 
 case "$log" in
-    *$major_string_token* ) new=$(semver -i major "$tag"); part="major";;
+    *$major_string_token* ) new=$(semver -i feat "$tag"); part="major";;
     *$minor_string_token* ) new=$(semver -i minor "$tag"); part="minor";;
     *$patch_string_token* ) new=$(semver -i patch "$tag"); part="patch";;
     *$none_string_token* )
